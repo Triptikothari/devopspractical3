c070e780f3d86720664a9691637bdf3c98937198
Tripti Kothari <triptikothari1999@gmail.com>
2021-05-23 12:26:53 +0530

    dd
    heelloo

---
+++ src/test/java/com/example/devopspractical3/Devopspractical3ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.devopspractical3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Devopspractical3ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
